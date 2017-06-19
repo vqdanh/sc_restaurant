df5dfc5e2d5dbadb262dacf89702778416583a91
danhvo <danh.vo@4tellus.com>
2017-06-19 07:01:30 +0700

    asset compile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617051456) do
+ActiveRecord::Schema.define(version: 20170618175739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
