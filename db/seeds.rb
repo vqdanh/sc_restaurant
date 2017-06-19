b10ed6297dc66774c81d8ab89a16d1dbf69012d1
danhvo <danh.vo@4tellus.com>
2017-06-19 07:40:41 +0700

    update db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,67 @@ if Menu.count == 0
 else
 	puts "You already have menu in the DB"
 end
+
+
+if Food.count == 0 
+	Food.create(title: "Bò Né", 
+		description: "",  
+		url: "http://lorempixel.com/400/400/food", 
+		menu_id: 1, 
+		price: 25) 
+
+	Food.create(title: "Bún Bò", 
+		description: "", 
+		url: "http://lorempixel.com/400/400/food", 
+		menu_id: 1, 
+		price: 20) 
+
+	Food.create(title: "Bánh Mì Sài Gòn", 
+		description: "", 
+		url: "http://lorempixel.com/400/400/food", 
+		menu_id: 1,
+		price: 10) 
+
+	Food.create(title: "Xôi", 
+		description: "", 
+		url: "http://lorempixel.com/400/400/food",
+		price: 15) 
+
+	Food.create(title: "Bánh Canh", 
+		description: "", 
+		url: "http://lorempixel.com/400/400/food", 
+		menu_id: 1, 
+		price: 25) 
+
+	Food.create(title: "Phở", 
+		description: "", 
+		url: "http://lorempixel.com/400/400/food", 
+		price: 30) 
+
+	Food.create(title: "Thịt Kho", description: "", 
+		url: "http://lorempixel.com/400/400/food", 
+		menu_id: 2, 
+		price: 30) 
+
+	Food.create(title: "Gà Xả Ớt", 
+		description: "", 
+		url: "http://lorempixel.com/400/400/food", 
+		menu_id: 2, 
+		price: 35) 
+
+	Food.create(title: "Pizza ", 
+		description: "", 
+		url: "http://lorempixel.com/400/400/food", 
+		menu_id: 2, 
+		price: 50) 
+
+
+	Food.create(title: "ShuShi", 
+		description: "", 
+		url: "http://lorempixel.com/400/400/food", 
+		menu_id: 3, 
+		price: 100)
+end
+else
+	puts "You already have food in the DB"
+end
